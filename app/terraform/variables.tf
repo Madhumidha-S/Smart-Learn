c61cfe2242a65d8383e11898c9a6c88941cb8b22
Madhumidha <madhumidha072005@gmail.com>
2025-10-24 19:04:17 +0530

    fix: terraform retry

--- app/terraform/variables.tf
+++ app/terraform/variables.tf
@@ -7,7 +7,7 @@ variable "db_password" { type = string }
 variable "email" { type = string }
 
 variable "client_id" {}
-variable "client_secret" {}
+variable "client_secret" { sensitive = true }
 variable "tenant_id" {}
 variable "subscription_id" {}
 
